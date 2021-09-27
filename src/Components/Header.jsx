d18219167c50fa283a286beb48669294ca99c212
evgenymozh <evgenymozhaev90@yandex.ru>
2021-09-27 20:33:44 +0500

    all files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+
+      <header className='header'>
+          <img src='logo1.png'></img>
+      </header>
+
+  )
+
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return(
+
+    <div className='profile'>
+      <img src='fon.jpg'></img>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My post
+      </div>
+      <div>
+        New posts
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+
+  )
+
+}
+
+export default Profile;
